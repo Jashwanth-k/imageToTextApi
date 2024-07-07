3c53d6e9f0a79c1d202fc5950b16c8a7b05ca150
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2024-07-07 12:46:36 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,24 @@
 from PIL import Image
 import pytesseract
 import flask
+import io
+import base64
 
 app = flask.Flask('api server')
 # app.config["DEBUG"] = True
 
-# image should to passed in formData with file key
 @app.route('/imgToText', methods=['POST'])
 def imgToText():
     try:
         req = flask.request
-        files = req.files
-        im = Image.open(files['file'])
+        base64_str = req.json['base64_str']
+        # file = req.form['file']
+        # im = Image.open(file)
+        im = Image.open(io.BytesIO(base64.decodebytes(bytes(base64_str, "utf-8"))))
         text = pytesseract.image_to_string(im, lang = 'eng')
         return { 'data' : text, 'status' : True }
-    except:
+    except Exception as e:
+        print(e)
         return {'msg' : 'something went wrong', 'status' : False}
 
 app.run()
